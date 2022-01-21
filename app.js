0317d607cf68aed43757f5d1f8aab48a31cc8826
inesiv <92948086+inesiv@users.noreply.github.com>
2022-01-21 21:30:54 +0200

    Update app.js
     Populated data structure items to list

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
-const ItemCtrl = (function (){
-    //Item Constructor
-    const Item = function (id, name, calories){
-        this.id = id
-        this.name = name
+@@ -10,7 +10,7 @@ const ItemCtrl = (function (){
         this.calories = calories
     }
 
     //Data Struckture
+    //Data Structure
     const data = {
         items:[
             {id: 0, name: 'Steak Dinner', calories: 1200},
-            {id: 0, name: ' Cookie', calories: 400},
-            {id: 0, name: 'Eggs', calories: 300},
-        ],
-        total: 0
+@@ -21,22 +21,47 @@ const ItemCtrl = (function (){
     }
 
     return {
+        getItems: function (){
+            return data.items
+        },
         logData: function (){
             return data
         }
@@ -26,6 +23,26 @@ const ItemCtrl = (function (){
 //UI Controller
 const UICtrl = (function (){
     console.log('UICtrl')
+const UICtrl = (function(){
+    return {
+    populateItemList: function(items){
+        //create html content
+        let html = '';
+
+        //parse data and create list items html
+        items.forEach(function(item){
+            html += `<li class="collection-item" id="item-${item.id}">
+        <strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+        <a href="#" class="secondary-content">
+            <i class="fas fa-pencil-alt"></i>
+        </a>
+        </li>`;
+        })
+
+        //insert list items
+        document.querySelector("#item-list").innerHTML = html;
+        }
+    }
 })();
 
 //App Controller
@@ -33,9 +50,11 @@ const App = (function (ItemCtrl, UICtrl){
     return {
         init: function (){
             console.log(ItemCtrl.logData())
+            console.log('Initializing App')
+            //fetch items from data structure
+            const items = ItemCtrl.getItems()
+            //populate items list
+            UICtrl.populateItemList(items)
         }
     }
 })(ItemCtrl, UICtrl)
-
-//Initialize App
-App.init() 
