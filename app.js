95a10420c5a543f540a6d1c58fe8a83d20e9a5e3
inesiv <92948086+inesiv@users.noreply.github.com>
2022-01-21 21:33:45 +0200

    Update app.js
     Added total calories

--- app.js
+++ app.js
@@ -51,3 +51,54 @@
         }
         console.log(input)
         event.preventDefault()
+    
+       // return new item
+            return newItem
+        },
+        getTotalCalories: function(){
+            let total = 0;
+            //loop through items and add calories
+            data.items.forEach(function(item){
+                total = total + item.calories;
+                console.log(total)
+            });
+            //set total calories in data structure
+            data.total = total;
+            console.log(data.total)
+            //return total
+            return data.total;
+        },
+        logData: function (){
+            return data
+        }
+@@ -54,7 +67,8 @@ const UICtrl = (function(){
+        itemList: '#item-list',
+        itemNameInput: '#item-name',
+        itemCaloriesInput: '#item-calories',
+        addBtn: '.add-btn'
+        addBtn: '.add-btn',
+        totalCalories: '.total-calories'
+    }
+    return {
+    populateItemList: function(items){
+@@ -104,6 +118,9 @@ const UICtrl = (function(){
+        clearInput: function (){
+        document.querySelector(UISelectors.itemNameInput).value = '';
+        document.querySelector(UISelectors.itemCaloriesInput).value = '';
+        },
+        showTotalCalories: function(totalCalories){
+        document.querySelector(UISelectors.totalCalories).textContent =totalCalories;
+        }
+    }
+})();
+@@ -126,6 +143,10 @@ const App = (function (ItemCtrl, UICtrl){
+           const newItem = ItemCtrl.addItem(input.name, input.calories)
+            // clear item to UI items list
+            UICtrl.addListItem(newItem)
+            //get total calories
+            const totalCalories = ItemCtrl.getTotalCalories();
+            //add total calories to UI
+            UICtrl.showTotalCalories(totalCalories);
+            //clear fields
+            UICtrl.clearInput();
+        }
